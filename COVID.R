e40a83b3d5d5ba637b6bd7270595951fef125f57
John W <scribe_airier_0v@icloud.com>
2024-12-08 20:56:03 -0330

    Corrected conditional statement

--- COVID.R
+++ COVID.R
@@ -11,9 +11,9 @@ options(scipen=999)
 data <- read_csv("https://health-infobase.canada.ca/src/data/covidLive/covid19-download.csv")
 
 # Transform!
-province <- "Newfoundland and Labrador"
+province <- "Nunavut"
 data <- transform(data, totalcases = as.numeric(totalcases))
-data <- data[data$totalcases > 0 & data$numdeaths >= 0,]
+data <- data[data$totalcases >= 0 & data$numdeaths >= 0,]
 data <- data[data$prname == province,]
 
 # Tidy!
