2d36e560df9a5b8b7547255499182f215cecd3a5
John W <scribe_airier_0v@icloud.com>
2024-12-08 23:33:23 -0330

    Changed conditional statement, plot labels, title of y axis

--- COVID.R
+++ COVID.R
@@ -12,7 +12,7 @@ data <- read_csv("https://health-infobase.canada.ca/src/data/covidLive/covid19-d
 
 # Transform!
 data <- transform(data, totalcases = as.numeric(totalcases))
-data <- data[data$totalcases > 0 & data$numdeaths >= 0,]
+data <- data[data$totalcases >= 0 & data$numdeaths >= 0,]
 data <- data[data$prname == "Newfoundland and Labrador",]
 
 # Tidy!
@@ -20,10 +20,10 @@ data <- na.omit(data)
 
 # Visualize!
 ggplot(data=data, mapping=aes(x=date,y=totalcases)) +
-  geom_line(mapping=aes(color="Cases"),linewidth=1) + 
-  geom_line(mapping=aes(x=date,y=numdeaths,color="Deaths"),linewidth=1) +
+  geom_line(mapping=aes(color="Total cases"),linewidth=1) + 
+  geom_line(mapping=aes(x=date,y=numdeaths,color="Total deaths"),linewidth=1) +
   scale_y_continuous(trans="log10") +
   scale_x_date(date_labels = "%Y",date_breaks="1 year") +
-  labs(x="Date",y="Total Count (Log. Scale)", title="Total COVID-19 Cases and Deaths") + 
+  labs(x="Date",y="Total (Log. Scale)", title="Total COVID-19 Cases and Deaths") + 
   facet_wrap(~prname) +
   scale_colour_colorblind(name="Legend")
