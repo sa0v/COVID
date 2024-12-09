f92b70749caf5957f5c8f7a4f39f263f80fb477d
John W <scribe_airier_0v@icloud.com>
2024-12-08 22:24:36 -0330

    Scale x axis by year

--- COVID.R
+++ COVID.R
@@ -23,6 +23,7 @@ ggplot(data=data, mapping=aes(x=date,y=totalcases)) +
   geom_line(mapping=aes(color="Cases"),linewidth=1) + 
   geom_line(mapping=aes(x=date,y=numdeaths,color="Deaths"),linewidth=1) +
   scale_y_continuous(trans="log10") +
+  scale_x_date(date_labels = "%Y",date_breaks="1 year") +
   labs(x="Date",y="Total Count (Log. Scale)", title="Total COVID-19 Cases and Deaths") + 
   facet_wrap(~prname) +
   scale_colour_colorblind(name="Legend")
